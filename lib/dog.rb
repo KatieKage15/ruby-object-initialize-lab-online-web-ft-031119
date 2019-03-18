a101df1dde53322e86dea55d03e675c25a55db2f
victorious-messaging-4309 <wildayk@gmail.com>
2019-03-18 15:36:26 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  def initialize(breed)
+    @bread = breed
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
+  if !@breed 
+    return "Mutt"
+  end
+end
+
